4ffe6bdcff12f8851ee1f9781952c560d1f3240c
Alex McKean <amckean@me.com>
2018-06-07 14:44:40 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,6 +5,10 @@ class SchoolClassesController < ApplicationController
     redirect_to school_class_path(@school_class)
   end
 
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
   def new
     @school_class = SchoolClass.new
   end
