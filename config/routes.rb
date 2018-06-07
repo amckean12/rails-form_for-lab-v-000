93d9bdac971b512a31d19d383a571086f0b4261d
Alex McKean <amckean@me.com>
2018-06-07 14:18:43 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :school_classes, only: [:index, :new, :create]
+  resources :school_classes, only: [:index, :new]
   get '/school_class/:id', to: 'school_classes#show', as: 'school_class'
 end
