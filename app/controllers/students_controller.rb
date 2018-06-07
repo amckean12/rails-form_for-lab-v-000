3a6c5ac6a79da23817a2a3a2d310c1f9eae61391
Alex McKean <amckean@me.com>
2018-06-07 14:35:52 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
   def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
   end
-  
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
 end
